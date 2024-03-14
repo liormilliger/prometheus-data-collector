e6882386fe9fdfa7412d3e17ef77bed6fc67430d
Lior <liormdevops@gmail.com>
2024-03-14 08:03:19 +0200

    HOME IP Changed in tfvars, Grafana my_dashboard with new visualization

--- app/app.py
+++ app/app.py
@@ -4,13 +4,13 @@ import requests
 app = Flask(__name__)
 
 # Environment Variables
-prometheus_url = "http://prometheus:9090/api/v1/"
+prometheus_api = "http://prometheus:9090/api/v1/"
 
 
 metrics = {
     "mem_usage": "query?query=node_memory_Active_bytes/node_memory_MemTotal_bytes*100",
     "cpu_usage": "query?query=(sum by (cpu)(rate(node_cpu_seconds_total{mode!='idle'}[5m]))*100)",
-    "disk_status": "query?query=(node_filesystem_avail_bytes/node_filesystem_size_bytes)*100"
+    "disk_status": "query?query=(1 - node_filesystem_avail_bytes/node_filesystem_size_bytes)*100"
 }
 
 @app.route('/', methods=['GET'])
@@ -19,7 +19,7 @@ def prometheus_service():
     metrics_data = {}
     
     for metric_name, metric_url in metrics.items():
-        response = requests.get(prometheus_url + metric_url)
+        response = requests.get(prometheus_api + metric_url)
         data = response.json()
         if data["status"] == "success":
             value = data["data"]["result"][0]["value"][1]
@@ -29,13 +29,11 @@ def prometheus_service():
     
     response_data = {
         'memory_usage': f'{float(metrics_data["mem_usage"]):.2f}%',
-        'used_disk_space': f'{float(metrics_data["disk_status"]):.2f}%',
-        'cpu_usage': f'{float(metrics_data["cpu_usage"])*100:.2f}% of CPU is in use'
+        'free_disk_space': f'{float(metrics_data["disk_status"]):.2f}%',
+        'cpu_usage': f'{float(metrics_data["cpu_usage"]):.2f}% of CPU is in use'
     }
 
     return jsonify(response_data)
-
-
   
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
