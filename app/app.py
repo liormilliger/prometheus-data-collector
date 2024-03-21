d2cae3eeae21f9fbf01a5972082d0616189c1b46
Barak Rosen <barrozz@gmail.com>
2024-03-21 13:51:02 +0200

    add var for query call     modify collecting data to get multiple values instead of single value in case of using several cpus, rams etc
    Signed-off-by: Barak Rosen <barrozz@gmail.com>

--- app/app.py
+++ app/app.py
@@ -5,35 +5,40 @@ app = Flask(__name__)
 
 # Environment Variables
 prometheus_api = "http://prometheus:9090/api/v1/"
-
+prom_query_call = "query?query="
 
 metrics = {
-    "mem_usage": "query?query=(node_memory_Active_bytes/node_memory_MemTotal_bytes*100)",
-    "cpu_usage": "query?query=(sum by (cpu)(rate(node_cpu_seconds_total{mode!='idle'}[5m]))*100)",
-    "disk_status": "query?query=  (1 -(node_filesystem_free_bytes  {mountpoint='/etc/hostname'}/ node_filesystem_size_bytes {mountpoint='/etc/hostname'})) * 100 "
+    "mem_usage": "(node_memory_Active_bytes/node_memory_MemTotal_bytes*100)",
+    "cpu_usage": "(sum by (cpu)(rate(node_cpu_seconds_total{mode!='idle'}[5m]))*100)",
+    "disk_status": "(1 -(node_filesystem_free_bytes  {mountpoint='/etc/hostname'}/ node_filesystem_size_bytes {mountpoint='/etc/hostname'})) * 100 "
 }
 
 @app.route('/', methods=['GET'])
 def prometheus_service():
-    
+
     metrics_data = {}
-    
+
     for metric_name, metric_url in metrics.items():
-        response = requests.get(prometheus_api + metric_url)
+        response = requests.get(prometheus_api + prom_query_call + metric_url)
         data = response.json()
+
         if data["status"] == "success":
-            value = data["data"]["result"][0]["value"][1]
-            metrics_data[metric_name] = value
+            values = [result["value"][1] for result in data["data"]["result"]]
+            # value = data["data"]["result"][:]["value"][1]
+
+            prefix = metric_name.split('_')[0]
+            res = ', '.join([f'{prefix}-{index}: {float(value):.2f}%' for index, value in enumerate(values)])
+            metrics_data[metric_name] = res
         else:
             metrics_data[metric_name] = "ERROR: Failed to retrieve system data from Prometheus"
-    
+
     response_data = {
-        'memory_usage': f'{float(metrics_data["mem_usage"]):.2f}%',
-        'free_disk_space': f'{float(metrics_data["disk_status"]):.2f}%',
-        'cpu_usage': f'{float(metrics_data["cpu_usage"]):.2f}% of CPU is in use'
+        'memory_usage': metrics_data["mem_usage"],
+        'free_disk_space': metrics_data["disk_status"],
+        'cpu_usage': metrics_data["cpu_usage"]
     }
 
     return jsonify(response_data)
-  
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
